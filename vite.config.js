d90fdbf9c4530ea7b22c617b12ac522c3bc1eb24
Lakshmichandanamarthala <lakshmichandanamarthala@gmail.com>
2025-12-22 23:07:03 +0530

    updated todo list

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/ToDoList_App/",
+  base: "/ToDo-List-App/",
   plugins: [react()],
 });
